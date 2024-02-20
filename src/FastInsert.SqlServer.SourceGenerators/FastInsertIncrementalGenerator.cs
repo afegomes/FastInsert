b531c9a13a9b758a2ea9404b7e27c34450baa500
Fabrício Gomes <afegomes@gmail.com>
2024-02-20 10:43:09 -0300

    Move MappingConfiguration

--- src/FastInsert.SqlServer.SourceGenerators/FastInsertIncrementalGenerator.cs
+++ src/FastInsert.SqlServer.SourceGenerators/FastInsertIncrementalGenerator.cs
@@ -380,4 +380,13 @@ namespace FastInsert.SqlServer.SourceGenerators
             return properties;
         }
     }
+
+    internal sealed class MappingConfiguration
+    {
+        public int Index { get; set; }
+
+        public string ColumnName { get; set; }
+
+        public string PropertyName { get; set; }
+    }
 }
